a19c66e9ad12e1ba96c32edbb36edd58f6c69603
Tarun Mehra <tmehra@tmehra.sjc.csb>
2018-01-23 14:18:54 -0800

    fix

--- vars/deployToTpaas.groovy
+++ vars/deployToTpaas.groovy
@@ -8,7 +8,7 @@ def call (body) {
 
 	def jenkinsToken = readFile('/etc/jenkins/token')
                 
-	openshiftDeploy apiURL: "${ocpUrl}", depCfg: config.microservice, namespace: config.namespace, authToken: "${jenkinsToken}",  verbose: 'true', waitTime: '', waitUnit: 'sec'
+	openshiftDeploy apiURL: config.ocpUrl, depCfg: config.microservice, namespace: config.namespace, authToken: "${jenkinsToken}",  verbose: 'true', waitTime: '', waitUnit: 'sec'
           		
     	echo "Verifying the deployment in TPASS..."
     	openshiftVerifyDeployment apiURL: config.ocpUrl, depCfg: config.microservice, namespace: config.namespace, authToken: "${jenkinsToken}", replicaCount: '1', verbose: 'true', verifyReplicaCount: 'true', waitTime: '900', waitUnit: 'sec'
