3054359b05a487740a2c058ef09da4d7f556c172
Tarun Mehra <tmehra@tmehra.sjc.csb>
2018-01-23 12:28:48 -0800

    fix

--- vars/processOcpDeploymentTemplates.groovy
+++ vars/processOcpDeploymentTemplates.groovy
@@ -13,7 +13,7 @@ def call (body) {
 	def jenkinsToken = readFile('/etc/jenkins/token')
 
 	//login to TPAAS cluster
-        pipelineUtils.login(ocpUrl, jenkinsToken)          
+        pipelineUtils.login(config.ocpUrl, jenkinsToken)          
 	
 	pipelineUtils.processOcpTemplates("pengg-openshift/pengg-openshift-system/openshift/templates/pengg-spog-dc.yml",
             "NAME=${config.microservice} APPLICATION_IS_TAG_WEB=${config.microservice}:1.7 APPLICATION_IS_TAG_API=${config.microservice}:${config.sourceRepositoryRef} APPLICATION_IS_NM_WEB=${config.namespace}", config.namespace)
