7b48d36fcfbc55c544f7e852824fb37ba55d0120
Tarun Mehra <tmehra@tmehra.sjc.csb>
2018-01-23 09:50:00 -0800

    fix

--- vars/applyBuildTemplates.groovy
+++ vars/applyBuildTemplates.groovy
@@ -6,7 +6,7 @@ def call (String gitRepoUrl, String sourceRepositoryRef, String gitPullSecret, S
  	def jenkinsToken = readFile('/etc/jenkins/token')
                 
 	//Process templates and start build
-        pipelineUtils.processTemplateAndStartBuild("pengg-openshift/pengg-openshift-system/openshift/templates/pengg-runtime-bc-spog.yaml", "BASE=${config.microservice} SOURCE_REPOSITORY_URL=${gitRepoUrl} SOURCE_REPOSITORY_REF=${sourceRepositoryRef} GIT_PULL_SECRET=${gitPullSecret} ANGULAR_HOME_DIR=web Version_File_Loc=version DEST_DEPLOY_NAMESPACE=${deployNamespace} authToken=${jenkinsToken} email_Address=${emailAddress}", buildNamespace, "${microservice}-${sourceRepositoryRef}")
+        pipelineUtils.processTemplateAndStartBuild("pengg-openshift/pengg-openshift-system/openshift/templates/pengg-runtime-bc-spog.yaml", "BASE=${config.microservice} SOURCE_REPOSITORY_URL=${gitRepoUrl} SOURCE_REPOSITORY_REF=${sourceRepositoryRef} GIT_PULL_SECRET=${gitPullSecret} ANGULAR_HOME_DIR=web Version_File_Loc=version DEST_DEPLOY_NAMESPACE=${deployNamespace} authToken=${jenkinsToken} email_Address=${emailAddress}", ${buildNamespace}, "${microservice}-${sourceRepositoryRef}")
 
  }
 
--- vars/checkoutOpenshiftTemplates.groovy
+++ vars/checkoutOpenshiftTemplates.groovy
@@ -3,6 +3,6 @@
 def call (String gitRepoUrl) {
 
     // Checkout openshift template files
-    checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: 'pengg-openshift']], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'a0abb0d8-4a01-4d4e-a4c7-90526325f245', url: ]]])
+    checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: 'pengg-openshift']], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'a0abb0d8-4a01-4d4e-a4c7-90526325f245', url: ${gitRepoUrl}]]])
 
 }
