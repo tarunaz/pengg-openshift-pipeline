14f7ce81ce9d2da1df864c0c095411abb0c189c5
Tarun Mehra <tmehra@tmehra.sjc.csb>
2018-01-23 09:54:33 -0800

    fix

--- vars/checkoutSource.groovy
+++ vars/checkoutSource.groovy
@@ -3,6 +3,6 @@
 def call (String gitRepoUrl) {
 
     // Checkout source code
-    checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: 'app-root']], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'a0abb0d8-4a01-4d4e-a4c7-90526325f245', url: ${gitRepoUrl}]]])
+    checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: 'app-root']], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'a0abb0d8-4a01-4d4e-a4c7-90526325f245', url: gitRepoUrl]]])
 
 }
