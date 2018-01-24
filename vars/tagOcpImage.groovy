de67ddbb52790c6684c5f11b53aabaeb9c72b7c3
Tarun Mehra <tmehra@tmehra.sjc.csb>
2018-01-24 09:24:16 -0800

    fix

--- vars/tagOcpImage.groovy
+++ vars/tagOcpImage.groovy
@@ -2,8 +2,10 @@
 
 def call (Map config) {
        
+	def branch = '*/' + config.sourceRepositoryRef
+
          // Checkout source code for version file
-    	checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: 'app-root']], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'a0abb0d8-4a01-4d4e-a4c7-90526325f245', url: gitRepoUrl]]])
+    	checkout([$class: 'GitSCM', branches: [[name: branch]], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: 'app-root']], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'a0abb0d8-4a01-4d4e-a4c7-90526325f245', url: config.gitRepoUrl]]])
 
 	echo "readFile version "	
         def VERSION = readFile 'app-root/version'
