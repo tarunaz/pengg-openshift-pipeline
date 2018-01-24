2d65d1a0dfd9ece0a4ce2997c20d87fa335222b3
Tarun Mehra <tmehra@tmehra.sjc.csb>
2018-01-24 10:09:53 -0800

    fix

--- vars/processOcpDeploymentTemplates.groovy
+++ vars/processOcpDeploymentTemplates.groovy
@@ -12,7 +12,7 @@ def call (Map config) {
         pipelineUtils.login(config.ocpUrl, jenkinsToken)          
 	
 	pipelineUtils.processOcpTemplates("pengg-openshift/pengg-openshift-system/openshift/templates/pengg-spog-dc.yml",
-            "NAME=${config.microservice} APPLICATION_IS_TAG_WEB=${config.microservice}:1.7 APPLICATION_IS_TAG_API=${config.microservice}:${config.sourceRepositoryRef} APPLICATION_IS_NM_WEB=${config.namespace}", config.deployNamespace)
+            "NAME=${config.microservice} APPLICATION_IS_TAG_WEB=${config.microservice}:1.7 APPLICATION_IS_TAG_API=${config.microservice}:${config.sourceRepositoryRef} APPLICATION_IS_NM_WEB=${config.deployNamespace}", config.deployNamespace)
 
  }
 
