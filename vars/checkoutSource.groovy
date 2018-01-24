908541da38afa4092cb02d57d00ce13d04d66fc9
Tarun Mehra <tmehra@tmehra.sjc.csb>
2018-01-23 16:25:09 -0800

    fix

--- vars/checkoutSource.groovy
+++ vars/checkoutSource.groovy
@@ -2,7 +2,7 @@
 
 def call (String gitRepoUrl) {
 
-    // Checkout source code
+    // Checkout source code version file
     checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: 'app-root']], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'a0abb0d8-4a01-4d4e-a4c7-90526325f245', url: gitRepoUrl]]])
 
 }
--- vars/tagOcpImage.groovy
+++ vars/tagOcpImage.groovy
@@ -2,6 +2,9 @@
 
 def call () {
        
+         // Checkout source code for version file
+    	checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: 'app-root']], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'a0abb0d8-4a01-4d4e-a4c7-90526325f245', url: gitRepoUrl]]])
+
 	echo "readFile version "	
         def VERSION = readFile 'app-root/version'
         
