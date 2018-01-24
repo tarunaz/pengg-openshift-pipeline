ff829f93817603542e89b224fb295307bf5d1164
Tarun Mehra <tmehra@tmehra.sjc.csb>
2018-01-23 17:54:53 -0800

    fix

---
+++ src/com/netapp/Constants.groovy
@@ -0,0 +1,8 @@
+package com.netapp
+
+class Constants {
+
+    static final MICROSERVICE = 'tarun-spog';
+    static final GIT_SRC_REPOSITORY_REF = 'master';
+  
+}
--- vars/processOcpBuildTemplates.groovy
+++ vars/processOcpBuildTemplates.groovy
@@ -6,7 +6,7 @@ def call (body) {
        def config = [:]
     	body.resolveStrategy = Closure.DELEGATE_FIRST
     	body.delegate = config
-  	  body()
+  	body()
                 
 	def pipelineUtils = new PipelineUtils()
 
