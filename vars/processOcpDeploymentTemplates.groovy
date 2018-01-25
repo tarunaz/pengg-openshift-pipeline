05823715e68de851cfb613352448931d184b39a8
Tarun Mehra <tmehra@tmehra.sjc.csb>
2018-01-25 09:35:22 -0800

    fix

--- vars/processOcpDeploymentTemplates.groovy
+++ vars/processOcpDeploymentTemplates.groovy
@@ -9,7 +9,7 @@ def call () {
 	def jenkinsToken = readFile('/etc/jenkins/token')
 
 	//login to TPAAS cluster
-        pipelineUtils.login(config.ocpUrl, jenkinsToken)          
+        pipelineUtils.login("${OCP_DEPLOY_URL}", jenkinsToken)          
 	
 	pipelineUtils.processOcpTemplates("${OCP_DEPLOY_TEMPLATES_LOC}",
             "NAME=${BASE} APPLICATION_IS_TAG_WEB=${BASE}:1.7 APPLICATION_IS_TAG_API=${BASE}:${SOURCE_REPOSITORY_REF} APPLICATION_IS_NM_WEB=${DEPLOY_NAMESPACE}", "${DEPLOY_NAMESPACE}")
