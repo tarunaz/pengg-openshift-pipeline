1d88b0f2903e16d5083d98ef436a048713a2fc92
Tarun Mehra <tmehra@tmehra.sjc.csb>
2018-01-24 18:14:11 -0800

    fix

--- vars/processOcpBuildTemplates.groovy
+++ vars/processOcpBuildTemplates.groovy
@@ -9,7 +9,7 @@ def call (Map config) {
 	def jenkinsToken = readFile('/etc/jenkins/token')
                 
 	//Process templates
-        pipelineUtils.processOcpTemplates("pengg-openshift/pengg-openshift-system/openshift/templates/pengg-runtime-bc-spog.yaml", "BASE=$BASE SOURCE_REPOSITORY_URL=$SOURCE_REPOSITORY_URL SOURCE_REPOSITORY_REF=${config.sourceRepositoryRef} GIT_PULL_SECRET=${config.gitPullSecret} ANGULAR_HOME_DIR=web Version_File_Loc=version DEST_DEPLOY_NAMESPACE=${config.deployNamespace} authToken=${jenkinsToken} email_Address=${config.emailAddress}", config.buildNamespace)
+        pipelineUtils.processOcpTemplates("pengg-openshift/pengg-openshift-system/openshift/templates/pengg-runtime-bc-spog.yaml", "BASE=${BASE} SOURCE_REPOSITORY_URL=${SOURCE_REPOSITORY_URL} SOURCE_REPOSITORY_REF=${config.sourceRepositoryRef} GIT_PULL_SECRET=${config.gitPullSecret} ANGULAR_HOME_DIR=web Version_File_Loc=version DEST_DEPLOY_NAMESPACE=${config.deployNamespace} authToken=${jenkinsToken} email_Address=${config.emailAddress}", config.buildNamespace)
 
 
  }
