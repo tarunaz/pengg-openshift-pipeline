5104df7dfa39df47e142b6914233ead7e83e2a16
Tarun Mehra <tmehra@tmehra.sjc.csb>
2018-01-24 18:58:53 -0800

    fix

--- vars/processOcpBuildTemplates.groovy
+++ vars/processOcpBuildTemplates.groovy
@@ -9,7 +9,7 @@ def call (Map config) {
 	def jenkinsToken = readFile('/etc/jenkins/token')
                 
 	//Process templates
-        pipelineUtils.processOcpTemplates("${OCP_BUILD_TEMPLATES_LOC}", "BASE=${BASE} SOURCE_REPOSITORY_URL=${SOURCE_REPOSITORY_URL} SOURCE_REPOSITORY_REF=${SOURCE_REPOSITORY_REF} GIT_PULL_SECRET=${GIT_PULL_SECRET} ANGULAR_HOME_DIR=web Version_File_Loc=version authToken=${jenkinsToken}", "${BUILD_NAMESPACE}")
+        pipelineUtils.processOcpTemplates("${OCP_BUILD_TEMPLATES_LOC}", "BASE=${BASE} SOURCE_REPOSITORY_URL=${SOURCE_REPOSITORY_URL} SOURCE_REPOSITORY_REF=${SOURCE_REPOSITORY_REF} GIT_PULL_SECRET=${GIT_PULL_SECRET} ANGULAR_HOME_DIR=web Version_File_Loc=version", "${BUILD_NAMESPACE}")
 
 
  }
