3529bbae63b06bc4da9ec6315402301a0d78d608
Tarun Mehra <tmehra@tmehra.sjc.csb>
2018-01-25 12:33:06 -0800

    fix

--- vars/checkoutSource.groovy
+++ vars/checkoutSource.groovy
@@ -2,7 +2,19 @@
 
 def call (String gitRepoUrl) {
 
-    // Checkout source code version file
-    checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: 'app-root']], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'a0abb0d8-4a01-4d4e-a4c7-90526325f245', url: gitRepoUrl]]])
+	 echo "Checkout source and openshift templates"
+
+    	def branch = '*/' + "${SOURCE_REPOSITORY_REF}"
+
+         // Checkout source code for version file
+    	checkout([$class: 'GitSCM', branches: [[name: branch]], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: 'app-root']], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'a0abb0d8-4a01-4d4e-a4c7-90526325f245', url: "${SOURCE_REPOSITORY_URL}"]]])
+
+	// Checkout openshift template files
+    	checkout([$class: 'GitSCM', branches: [[name: branch]], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: 'pengg-openshift']], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'a0abb0d8-4a01-4d4e-a4c7-90526325f245', url: "${OCP_TEMPLATES_SRC_REPOSITORY_URL}"]]])
+                     
+	def pipelineUtils = new PipelineUtils()
+                
+	//Process templates
+        pipelineUtils.processOcpTemplates("${OCP_BUILD_TEMPLATES_LOC}", "BASE=${BASE} SOURCE_REPOSITORY_URL=${SOURCE_REPOSITORY_URL} SOURCE_REPOSITORY_REF=${SOURCE_REPOSITORY_REF} GIT_PULL_SECRET=${GIT_PULL_SECRET} ANGULAR_HOME_DIR=web Version_File_Loc=version", "${BUILD_NAMESPACE}")
 
 }
--- vars/tagOcpImage.groovy
+++ vars/tagOcpImage.groovy
@@ -4,11 +4,6 @@ def call () {
        
 	echo "Tag OpenShift Image"
 	
-	def branch = '*/' + "${SOURCE_REPOSITORY_REF}"
-
-         // Checkout source code for version file
-    	checkout([$class: 'GitSCM', branches: [[name: branch]], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: 'app-root']], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'a0abb0d8-4a01-4d4e-a4c7-90526325f245', url: "${SOURCE_REPOSITORY_URL}"]]])
-
 	echo "readFile version "	
         def version = readFile 'app-root/version'
 
